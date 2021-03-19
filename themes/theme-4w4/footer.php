7e523791779ba1f4424ed6f4281b8c9f149c70d8
Finder-00 <arthurrobitaille@protonmail.com>
2021-03-19 15:48:35 -0400

    fini footer

--- themes/theme-4w4/footer.php
+++ themes/theme-4w4/footer.php
@@ -13,18 +13,18 @@
 
 	<footer id="colophon" class="site-footer">
 		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://cmaisonneuve.qc.ca', 'theme-4w4' ) ); ?>">
+			<h3>
 				<?php
 				/* translators: %s: CMS name, i.e. WordPress. */
-				printf( esc_html__( 'Fait par %s', 'theme-4w4' ), 'Arthur Robitaille' );
+				printf( 'Fait par Arthur Robitaille', 'theme-4w4' );
 				?>
-			</a>
+			</h3>
 			<span class="sep"> | </span>
 				<?php
 				/* translators: 1: Theme name, 2: Theme author. */
 				printf( esc_html__( 'Theme: %1$s by %2$s.', 'theme-4w4' ), 'arturo', '<a href="http://underscores.me/">finder</a>' );
 				?>
-				<a href="https://cmaisonneuve.qc.ca">liens pour le College de maisonnneuve</a>
+				<a href="https://cmaisonneuve.qc.ca">Liens pour le College de maisonnneuve</a>
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
